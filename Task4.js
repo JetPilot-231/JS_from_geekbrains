7e9195fd3acc21f0f50d1b0f958f642aa656e064
Anton <jetpilot-231@rambler.ru>
2020-07-22 17:12:42 +0300

    Lesson1

---
+++ Task4.js
@@ -0,0 +1,10 @@
+"use strict"
+
+let mode = "normal";
+let my_valu3 = 102;
+//let 3my_value3 = "102";
+let __hello__ = "world";
+let $$$ = "money";
+//let !0_world = true;
+
+// 3my_value3 и !0_world являются недопустимыми значениями, так как начинется с цифры, второй с запрещённого символа
